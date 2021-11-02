01de5a1bbcb163f1421dd4498873a55c7a1042a8
lehai4 <chihaile4@gmail.com>
2021-11-02 20:48:44 +0700

    demo-angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Calculator App';
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
+  add() {
+    this.result = this.num1 + this.num2;
+  }
+  substract() {
+    this.result = this.num1 - this.num2;
+  }
+  multiply() {
+    this.result = this.num1 * this.num2;
+  }
+  divide() {
+    this.result = this.num1 / this.num2;
+  }
+  remainder() {
+    this.result = this.num1 % this.num2;
+  }
+}
